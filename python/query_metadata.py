ede25a08c8b7f4f587cca7a6c197a8791345619e
CYWCC <cyw9620@gmail.com>
2024-12-04 14:27:44 +0800

    add query_metadate.py

---
+++ python/query_metadata.py
@@ -0,0 +1,141 @@
+radar_metadata = {
+    # bc - Basketball Court (Handheld)
+    '20230920/1': {'Place': 'Basketball Court', 'Platform': 'Handheld', 'Distance/Duration': '84/74',
+                   'Weather': 'light rain', 'Lighting': 'night'},
+    '20230921/2': {'Place': 'Basketball Court', 'Platform': 'Handheld', 'Distance/Duration': '59/83',
+                   'Weather': 'moderate rain', 'Lighting': 'daytime'},
+
+    # bc - Basketball Court (Ebike)
+    '20231007/4': {'Place': 'Basketball Court', 'Platform': 'Ebike', 'Distance/Duration': '78/51', 'Weather': 'clear',
+                   'Lighting': 'daytime'},
+    '20231105/6': {'Place': 'Basketball Court', 'Platform': 'Ebike', 'Distance/Duration': '251/144',
+                   'Weather': 'moderate rain', 'Lighting': 'daytime'},
+    '20231105_aft/2': {'Place': 'Basketball Court', 'Platform': 'Ebike', 'Distance/Duration': '167/104',
+                       'Weather': 'light rain', 'Lighting': 'daytime'},
+
+    # sl - Starlake (Handheld)
+    '20230920/2': {'Place': 'Starlake', 'Platform': 'Handheld', 'Distance/Duration': '2182/1839',
+                   'Weather': 'light rain', 'Lighting': 'night'},
+    '20230921/3': {'Place': 'Starlake', 'Platform': 'Handheld', 'Distance/Duration': '2171/1857',
+                   'Weather': 'moderate rain', 'Lighting': 'daytime'},
+    '20230921/5': {'Place': 'Starlake', 'Platform': 'Handheld', 'Distance/Duration': '2015/1731',
+                   'Weather': 'moderate rain', 'Lighting': 'daytime'},
+
+    # sl - Starlake (Ebike)
+    '20231007/2': {'Place': 'Starlake', 'Platform': 'Ebike', 'Distance/Duration': '1997/657', 'Weather': 'clear',
+                   'Lighting': 'daytime'},
+    '20231019/1': {'Place': 'Starlake', 'Platform': 'Ebike', 'Distance/Duration': '1919/460', 'Weather': 'clear',
+                   'Lighting': 'night'},
+    '20231105/2': {'Place': 'Starlake', 'Platform': 'Ebike', 'Distance/Duration': '2045/524', 'Weather': 'heavy rain',
+                   'Lighting': 'daytime'},
+    '20231105/3': {'Place': 'Starlake', 'Platform': 'Ebike', 'Distance/Duration': '2069/537', 'Weather': 'heavy rain',
+                   'Lighting': 'daytime'},
+    '20231105_aft/4': {'Place': 'Starlake', 'Platform': 'Ebike', 'Distance/Duration': '2019/698',
+                       'Weather': 'light rain', 'Lighting': 'daytime'},
+    '20231109/3': {'Place': 'Starlake', 'Platform': 'Ebike', 'Distance/Duration': '1983/546', 'Weather': 'clear',
+                   'Lighting': 'daytime'},
+
+    # ss - Software School (Handheld)
+    '20230921/4': {'Place': 'Software School', 'Platform': 'Handheld', 'Distance/Duration': '736/622',
+                   'Weather': 'light rain', 'Lighting': 'daytime'},
+
+    # ss - Software School (Ebike)
+    '20231019/2': {'Place': 'Software School', 'Platform': 'Ebike', 'Distance/Duration': '781/533', 'Weather': 'clear',
+                   'Lighting': 'night'},
+    '20231105/4': {'Place': 'Software School', 'Platform': 'Ebike', 'Distance/Duration': '895/395',
+                   'Weather': 'heavy rain', 'Lighting': 'daytime'},
+    '20231105/5': {'Place': 'Software School', 'Platform': 'Ebike', 'Distance/Duration': '967/400',
+                   'Weather': 'moderate rain', 'Lighting': 'daytime'},
+    '20231105_aft/5': {'Place': 'Software School', 'Platform': 'Ebike', 'Distance/Duration': '826/534',
+                       'Weather': 'light rain', 'Lighting': 'daytime'},
+    '20231109/4': {'Place': 'Software School', 'Platform': 'Ebike', 'Distance/Duration': '795/533', 'Weather': 'clear',
+                   'Lighting': 'daytime'},
+
+    # if - Info Faculty (SUV)
+    '20231208/4': {'Place': 'Info Faculty', 'Platform': 'SUV', 'Distance/Duration': '2228/515', 'Weather': 'clear',
+                   'Lighting': 'daytime'},
+    '20231213/4': {'Place': 'Info Faculty', 'Platform': 'SUV', 'Distance/Duration': '2227/494', 'Weather': 'light rain',
+                   'Lighting': 'night'},
+    '20231213/5': {'Place': 'Info Faculty', 'Platform': 'SUV', 'Distance/Duration': '2225/475', 'Weather': 'light rain',
+                   'Lighting': 'night'},
+    '20240115/3': {'Place': 'Info Faculty', 'Platform': 'SUV', 'Distance/Duration': '2223/525', 'Weather': 'dusk',
+                   'Lighting': 'daytime'},
+    '20240116/5': {'Place': 'Info Faculty', 'Platform': 'SUV', 'Distance/Duration': '2228/514', 'Weather': 'dusk',
+                   'Lighting': 'daytime'},
+    '20240116_eve/5': {'Place': 'Info Faculty', 'Platform': 'SUV', 'Distance/Duration': '2224/462', 'Weather': 'clear',
+                       'Lighting': 'night'},
+    '20240123/3': {'Place': 'Info Faculty', 'Platform': 'SUV', 'Distance/Duration': '2231/535', 'Weather': 'clear',
+                   'Lighting': 'daytime'},
+
+    # iaf - Info and Arts Faculty (SUV)
+    '20231201/2': {'Place': 'Info and Arts Faculty', 'Platform': 'SUV', 'Distance/Duration': '4620/1042',
+                   'Weather': 'clear', 'Lighting': 'daytime'},
+    '20231201/3': {'Place': 'Info and Arts Faculty', 'Platform': 'SUV', 'Distance/Duration': '4631/946',
+                   'Weather': 'clear', 'Lighting': 'daytime'},
+    '20231208/5': {'Place': 'Info and Arts Faculty', 'Platform': 'SUV', 'Distance/Duration': '4616/870',
+                   'Weather': 'clear', 'Lighting': 'daytime'},
+    '20231213/2': {'Place': 'Info and Arts Faculty', 'Platform': 'SUV', 'Distance/Duration': '4603/938',
+                   'Weather': 'light rain', 'Lighting': 'night'},
+    '20231213/3': {'Place': 'Info and Arts Faculty', 'Platform': 'SUV', 'Distance/Duration': '4613/875',
+                   'Weather': 'light rain', 'Lighting': 'night'},
+    '20240113/2': {'Place': 'Info and Arts Faculty', 'Platform': 'SUV', 'Distance/Duration': '4610/1005',
+                   'Weather': 'clear', 'Lighting': 'daytime'},
+    '20240113/3': {'Place': 'Info and Arts Faculty', 'Platform': 'SUV', 'Distance/Duration': '4613/962',
+                   'Weather': 'clear', 'Lighting': 'daytime'},
+    '20240116_eve/4': {'Place': 'Info and Arts Faculty', 'Platform': 'SUV', 'Distance/Duration': '4609/868',
+                       'Weather': 'clear', 'Lighting': 'night'},
+
+    # iaef - Info, Arts, and Engineering Faculty (SUV)
+    '20240113/5': {'Place': 'Info, Arts, and Engineering Faculty', 'Platform': 'SUV', 'Distance/Duration': '7283/1509',
+                   'Weather': 'clear', 'Lighting': 'daytime'},
+    '20240115/2': {'Place': 'Info, Arts, and Engineering Faculty', 'Platform': 'SUV', 'Distance/Duration': '6641/1374',
+                   'Weather': 'clear', 'Lighting': 'daytime'},
+    '20240116/4': {'Place': 'Info, Arts, and Engineering Faculty', 'Platform': 'SUV', 'Distance/Duration': '6648/1515',
+                   'Weather': 'clear', 'Lighting': 'daytime'},
+
+    # st - Starlake Tower (SUV)
+    '20231208/1': {'Place': 'Starlake Tower', 'Platform': 'SUV', 'Distance/Duration': '275/147', 'Weather': 'clear',
+                   'Lighting': 'daytime'},
+    '20231213/1': {'Place': 'Starlake Tower', 'Platform': 'SUV', 'Distance/Duration': '276/126',
+                   'Weather': 'light rain', 'Lighting': 'night'},
+    '20240113/1': {'Place': 'Starlake Tower', 'Platform': 'SUV', 'Distance/Duration': '541/214', 'Weather': 'clear',
+                   'Lighting': 'daytime'},
+
+    # 81r - August 1 Road (SUV)
+    '20240116/2': {'Place': 'August 1 Road', 'Platform': 'SUV', 'Distance/Duration': '8554/1433',
+                   'Weather': 'light rain', 'Lighting': 'daytime'},
+    '20240116_eve/3': {'Place': 'August 1 Road', 'Platform': 'SUV', 'Distance/Duration': '8521/1293',
+                       'Weather': 'clear', 'Lighting': 'night'},
+    '20240123/2': {'Place': 'August 1 Road', 'Platform': 'SUV', 'Distance/Duration': '8539/1743', 'Weather': 'clear',
+                   'Lighting': 'daytime'}
+}
+
+
+def query_radar_metadata(condition):
+    results = []
+
+    if isinstance(condition, str) and condition in radar_metadata:
+        return radar_metadata[condition]
+
+    for data_id, data in radar_metadata.items():
+        match = True
+        for key, value in condition.items():
+            if data.get(key) != value:
+                match = False
+                break
+        if match:
+            results.append(data_id)
+    return results
+
+
+# example 1: query metadata for sequence
+sequence_data = query_radar_metadata('20230920/1')
+print("Metadata for sequence '20230920/1':", sequence_data)
+
+# # example 2: query metadata for sequences with single condition
+light_rain_data = query_radar_metadata({'Weather': 'moderate rain'})
+print("Light rain data:", light_rain_data)
+
+# example 3: query metadata for sequences with multiple conditions
+suv_light_rain_data = query_radar_metadata({'Platform': 'SUV', 'Weather': 'light rain'})
+print("SUV with light rain data:", suv_light_rain_data)
