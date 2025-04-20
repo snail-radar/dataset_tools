f69ab142dbfaef403ba1410e4963e807d89db0f0
CYWCC <cyw9620@gmail.com>
2025-04-20 22:49:26 +0800

    update Body_T_Zed2LeftCam.m

--- matlab/Body_T_Zed2LeftCam.m
+++ matlab/Body_T_Zed2LeftCam.m
@@ -1,27 +1,41 @@
-function T = Body_T_Zed2LeftCam()
+function T = Body_T_Zed2LeftCam(platform)
 % A. manually measured
 R_body_leftcam = [0, 0, 1; -1, 0, 0; 0, -1, 0];
 p_body_leftcam = [0.125; 0.07; -0.195];
 T = [R_body_leftcam p_body_leftcam; 0, 0, 0, 1];
 
-% B. calculated from /zed2i/zed_node/left_cam_imu_transform
-% zed2leftcamx the left camera frame with forward left up orientation
-% zed2imu the imu frame with forward right down orientation
-tx = -0.0020000000949949026;
-ty = -0.023000003769993782;
-tz = 0.0002200000308221206;
-qx = -0.0008717564051039517;
-qy = -0.00139715860132128;
-qz = -0.0010711626382544637;
-qw = 0.9999980330467224;
-q_zed2leftcamx_zed2imu = [qw, qx, qy, qz];
-T_zed2leftcamx_zed2imu = eye(4);
-% do not use quat2dcm from the aerospace toolbox which is different in convention.
-T_zed2leftcamx_zed2imu(1:3, 1:3) = quat2rotm(q_zed2leftcamx_zed2imu);
-T_zed2leftcamx_zed2imu(1:3, 4) = [tx; ty; tz];
+% B. calculated by Direct_visual_lidar_calib 
+% read Body_T_xt32
+Body_T_xt32 = Body_T_Xt32();
 
-T_rdf_zed2leftcamx = eye(4);
-T_rdf_zed2leftcamx(1:3, 1:3) = [0, -1, 0; 0, 0, -1; 1, 0, 0];
-T_zed2leftcam_imu = T_rdf_zed2leftcamx * T_zed2leftcamx_zed2imu;
-T = Body_T_Zed2Imu() * inv(T_zed2leftcam_imu);
+% select xt32_t_Cam based on platform and compute initial Body_T_LeftCam ---
+switch lower(platform)
+    case 'suv'
+        % SUV platform: fill in your real translation (m) and quaternion (qx,qy,qz,qw)
+        tx = 0.0695283917427731;    % X offset of xt32 in camera frame
+        ty = -0.008381612991474873;    % Y offset
+        tz = -0.17223038663727022;    % Z offset
+        qx = 0.019635536507920586;  % quaternion x
+        qy = 0.7097335839994078;  % quaternion y
+        qz = -0.7039714840647372;  % quaternion z
+        qw = -0.017799861603211602; % quaternion w
+    otherwise  
+        % Other platforms: fill in your real translation & quaternion
+        tx = 0.07493894778041606;
+        ty = -0.16471796028449764;
+        tz = -0.09812580091216104;
+        qx = -0.007712187968660904;
+        qy = -0.698602566500298;
+        qz = 0.7154675436354508;
+        qw = 0.0010817764035590007;
+end
+
+% build 4×4 Cam_T_xt32
+Cam_T_xt32 = eye(4);
+Cam_T_xt32(1:3,1:3) = quat2rotm([qw, qx, qy, qz]);  % [w x y z]
+Cam_T_xt32(1:3,4)   = [tx; ty; tz];
+
+% invert to init Body_T_Zed2LeftCam
+T = Body_T_xt32 * inv(Cam_T_xt32);
+
 end
